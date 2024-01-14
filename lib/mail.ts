dfd7f8f84831807bc7aa23e8018248ab027fce3a
Berke Güleryüz <berkeguleryuz@Berke-MacBook-Pro.local>
2024-01-14 16:47:28 +0300

    v.1.0

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "berke@clodron.com",
     to: email,
     subject: "2FA authentication",
     html: `<p>2FA code: ${token}</p>`,
@@ -17,7 +17,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "berke@clodron.com",
     to: email,
     subject: "Reset your password",
     html: `<p><a href="${resetLink}">Reset your password</a></p>`,
@@ -28,7 +28,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "berke@clodron.com",
     to: email,
     subject: "Confirm your email",
     html: `<p><a href="${confirmLink}">Confirm your email</a></p>`,
